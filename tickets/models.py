f4ee47d023f176e4f2ada73c7f9152737690020f
Ardan <aulia.rahman71@ui.ac.id>
2024-07-31 13:15:57 +0700

    Further update

--- tickets/models.py
+++ tickets/models.py
@@ -1,23 +1,141 @@
+from PIL import Image
 from django.contrib.auth.models import User
 from django.db import models
+
+
+class workshops_page(models.Model):
+    text_section_one = models.TextField(blank=False, default="Sample Description")
+    text_section_two = models.TextField(blank=False, default="Sample Description")
+    image_section_two_top_left = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_top_right = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_bot_left = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_bot_right = models.ImageField(upload_to='seminars_page_images/', )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        image_fields = [
+            self.image_section_two_top_left,
+            self.image_section_two_top_right,
+            self.image_section_two_bot_left,
+            self.image_section_two_bot_right,
+        ]
+
+        for image_field in image_fields:
+            if image_field:
+                self.compress_image(image_field)
+
+    def compress_image(self, image_field):
+        img = Image.open(image_field.path)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        img.save(image_field.path, 'JPEG', quality=85, optimize=True)
+
+
+class seminars_page(models.Model):
+    text_section_one = models.TextField(blank=False, default="Sample Description")
+    text_section_two = models.TextField(blank=False, default="Sample Description")
+    image_section_two_top_left = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_top_right = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_bot_left = models.ImageField(upload_to='seminars_page_images/', )
+    image_section_two_bot_right = models.ImageField(upload_to='seminars_page_images/', )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        image_fields = [
+            self.image_section_two_top_left,
+            self.image_section_two_top_right,
+            self.image_section_two_bot_left,
+            self.image_section_two_bot_right,
+        ]
+
+        for image_field in image_fields:
+            if image_field:
+                self.compress_image(image_field)
+
+    def compress_image(self, image_field):
+        img = Image.open(image_field.path)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        img.save(image_field.path, 'JPEG', quality=85, optimize=True)
 
 
 class landing_page(models.Model):
     header_section_one = models.TextField(blank=False, default="Sample Description")
     text_section_one = models.TextField(blank=False, default="Sample Description")
-    image_section_one = models.ImageField(upload_to='landingpage_images/',)
+    image_section_one = models.ImageField(upload_to='landingpage_images/', )
     header_section_two = models.CharField(max_length=100, blank=False, default="Sample Description")
     text_section_two = models.TextField(blank=False, default="Sample Description")
-    image_section_two_left = models.ImageField(upload_to='landingpage_images/',)
+    image_section_two_left = models.ImageField(upload_to='landingpage_images/', )
     image_section_two_header_left = models.CharField(max_length=100, blank=False, default="Sample Description")
     image_section_two_text_left = models.CharField(max_length=100, blank=False, default="Sample Description")
-    image_section_two_right = models.ImageField(upload_to='landingpage_images/',)
+    image_section_two_right = models.ImageField(upload_to='landingpage_images/', )
     image_section_two_header_right = models.CharField(max_length=100, blank=False, default="Sample Description")
     image_section_two_text_right = models.CharField(max_length=100, blank=False, default="Sample Description")
     header_section_three = models.TextField(blank=False, default="Sample Description")
     text_section_three = models.TextField(blank=False, default="Sample Description")
-    image_section_three_left = models.ImageField(upload_to='landingpage_images/',)
-    image_section_three_right = models.ImageField(upload_to='landingpage_images/',)
+    image_section_three_left = models.ImageField(upload_to='landingpage_images/', )
+    image_section_three_right = models.ImageField(upload_to='landingpage_images/', )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        image_fields = [
+            self.image_section_one,
+            self.image_section_two_left,
+            self.image_section_two_right,
+            self.image_section_three_left,
+            self.image_section_three_right
+        ]
+
+        for image_field in image_fields:
+            if image_field:
+                self.compress_image(image_field)
+
+    def compress_image(self, image_field):
+        img = Image.open(image_field.path)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        img.save(image_field.path, 'JPEG', quality=85, optimize=True)
+
+
+class about_us(models.Model):
+    text_section_one = models.TextField(blank=False, default="Sample Description")
+    header_section_two = models.CharField(max_length=100, blank=False, default="Sample Description")
+    subheader_section_two_left = models.CharField(max_length=100, blank=False, default="Sample Description")
+    text_section_two_left = models.TextField(blank=False, default="Sample Description")
+    subheader_section_two_right = models.CharField(max_length=100, blank=False, default="Sample Description")
+    text_section_two_right = models.TextField(blank=False, default="Sample Description")
+    image_section_two = models.ImageField(upload_to='about_us_images/', )
+    whatsapp_number = models.CharField(max_length=100, blank=False, default="62812345678  tanpa +",
+                                       help_text="+62812345678 tanpa +")
+    phone_number = models.CharField(max_length=100, blank=False, default="+62812345678", help_text="+62812345678")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        image_fields = [
+            self.image_section_two,
+        ]
+
+        for image_field in image_fields:
+            if image_field:
+                self.compress_image(image_field)
+
+    def compress_image(self, image_field):
+        img = Image.open(image_field.path)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        img.save(image_field.path, 'JPEG', quality=85, optimize=True)
 
 
 class Seminar(models.Model):
--- tickets/signals.py
+++ tickets/signals.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import Profile, landing_page
+from .models import Profile, landing_page, seminars_page, about_us
 
 
 @receiver(post_save, sender=User)
@@ -26,6 +26,35 @@ def delete_media_files(sender, instance, **kwargs):
         instance.image_section_two_right,
         instance.image_section_three_left,
         instance.image_section_three_right
+    ]
+
+    for image_field in image_fields:
+        if image_field:
+            image_path = image_field.path
+            if os.path.isfile(image_path):
+                os.remove(image_path)
+
+
+@receiver(post_delete, sender=seminars_page)
+def delete_media_files_seminars(sender, instance, **kwargs):
+    image_fields = [
+        instance.image_section_two_top_left,
+        instance.image_section_two_top_right,
+        instance.image_section_two_bot_left,
+        instance.image_section_two_bot_right,
+    ]
+
+    for image_field in image_fields:
+        if image_field:
+            image_path = image_field.path
+            if os.path.isfile(image_path):
+                os.remove(image_path)
+
+
+@receiver(post_delete, sender=about_us)
+def delete_media_files_about_us(sender, instance, **kwargs):
+    image_fields = [
+        instance.image_section_two,
     ]
 
     for image_field in image_fields:
