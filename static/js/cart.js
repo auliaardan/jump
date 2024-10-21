00cb308f86aeae47bb5e305b53666e99b748fbdb
Ardan <aulia.rahman71@ui.ac.id>
2024-10-22 00:32:42 +0700

    QoL changes

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,5 @@
 function updateCartItemCount() {
-    fetch("{% url 'cart_item_count' %}")
+    fetch(cartItemCountUrl)
         .then(response => response.json())
         .then(data => {
             document.getElementById('cart-item-count').innerText = data.item_count;
