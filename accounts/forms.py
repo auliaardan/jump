a6e3d1b8aaf2c1aa91519c9091512cc58db95800
Ardan <aulia.rahman71@ui.ac.id>
2024-10-22 11:22:34 +0700

    QoL changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,29 +1,41 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = (
             "username",
-            "first_name",
-            "last_name",
+            "nama_lengkap",
+            "nik",
             "institution",
             "email",
-            "phone_number",
+            "Nomor_telpon",
         )
-
+        help_texts = {
+            'nama_lengkap': 'Sesuai KTP beserta gelar lengkap',
+        }
+        widgets = {
+            'nama_lengkap': forms.TextInput(attrs={'placeholder': 'Nama lengkap sesuai KTP beserta gelar'}),
+        }
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = (
             "username",
-            "first_name",
-            "last_name",
+            "nama_lengkap",
+            "nik",
             "institution",
             "email",
-            "phone_number",
+            "Nomor_telpon",
         )
+        help_texts = {
+            'nama_lengkap': 'Sesuai KTP beserta gelar lengkap',
+            'email': 'Sesuai yang digunakan untuk plataran sehat'
+        }
+        widgets = {
+            'nama_lengkap': forms.TextInput(attrs={'placeholder': 'Nama lengkap sesuai KTP beserta gelar'}),
+        }
--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     institution = models.CharField(max_length=50, null=True, blank=False)
-    phone_number = models.CharField(max_length=50, null=True, blank=False)
+    Nomor_telpon = models.CharField(max_length=50, null=True, blank=False)
+    nama_lengkap = models.CharField(max_length=500, null=True, blank=False)
+    nik = models.CharField(max_length=50, null=False, blank=False)
+
