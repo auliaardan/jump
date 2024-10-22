f139cf2c04ce8a894b8082246b1f93d031b58a3d
Ardan <aulia.rahman71@ui.ac.id>
2024-10-22 11:24:28 +0700

    QoL changes

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,5 @@ class CustomUser(AbstractUser):
     institution = models.CharField(max_length=50, null=True, blank=False)
     Nomor_telpon = models.CharField(max_length=50, null=True, blank=False)
     nama_lengkap = models.CharField(max_length=500, null=True, blank=False)
-    nik = models.CharField(max_length=50, null=False, blank=False)
+    nik = models.CharField(max_length=50, null=True, blank=False)
 
