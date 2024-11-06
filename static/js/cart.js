90c4dea5b433418ab7e0799c0bc29acb38c7d45f
Ardan <aulia.rahman71@ui.ac.id>
2024-11-06 12:08:41 +0700

    Fixed several tings

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,7 +4,6 @@ function updateCartItemCount() {
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('cart-item-count').innerText = data.item_count;
     });
 }
 document.addEventListener('DOMContentLoaded', function () {
