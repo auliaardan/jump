20c82b3bc7ddf74414ebb0ebd5cd094b8049c401
Ardan <aulia.rahman71@ui.ac.id>
2025-01-19 13:18:38 +0700

    www. Typo

--- jump_project/settings.py
+++ jump_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '103.163.138.124', 'wwww.jump2025.com', 'jump2025.com',
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '103.163.138.124', 'www.jump2025.com', 'jump2025.com',
                  'jakartaurologymedicalupdate.com',
                  'jakartaurologymedicalupdate.id']
 
