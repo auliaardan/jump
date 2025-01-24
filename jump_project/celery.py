22d72847712182a31689b18494b938a26d6d17e9
Ardan <aulia.rahman71@ui.ac.id>
2025-01-24 14:07:51 +0700

    Added celery app?

--- jump_project/celery.py
+++ jump_project/celery.py
@@ -1,22 +1,9 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-import tickets.tasks
+from tickets import tasks
 
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jump_project.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jump_project.settings')
 
 app = Celery("jump_project")
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# Namespace 'CELERY' means all celery-related config keys
-# should have a `CELERY_` prefix.
-app.config_from_object("django.conf:settings", namespace="CELERY")
-
-# Auto-discover tasks from installed apps
+app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
