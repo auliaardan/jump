5c89d3c8edd6de2f2c25b3facb74f7f34d5583b9
Ardan <aulia.rahman71@ui.ac.id>
2025-01-26 06:30:13 +0700

    last chance with celery

--- tickets/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from .models import CartItem
-from datetime import timedelta
-from django.utils.timezone import now
-
-@shared_task
-def remove_expired_cartitems():
-    threshold = now() - timedelta(hours=24)
-    CartItem.objects.filter(created_at__lt=threshold).delete()
