111b1cfcbbecc79c0650e999f7fa16771beeea8b
Ardan <aulia.rahman71@ui.ac.id>
2026-01-30 18:24:25 +0700

    updated settings

--- jump_project/settings.py
+++ jump_project/settings.py
@@ -122,8 +122,8 @@ else:
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-            "PORT": os.getenv("DB_PORT", "3306"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
         }
     }
 
