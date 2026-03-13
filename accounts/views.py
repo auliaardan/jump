8d4d1763b7869f6543ca7c6c1de849c5cd8f19e9
Ardan <aulia.rahman71@ui.ac.id>
2026-03-13 15:03:05 +0700

    Edited password again

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import UpdateView
+from django.views.generic.edit import FormView
 
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -26,5 +27,19 @@ class CompleteProfileView(LoginRequiredMixin, UpdateView):
 
 
 class SitePasswordResetView(PasswordResetView):
-    domain_override = settings.SITE_DOMAIN
     extra_email_context = {"site_name": settings.SITE_NAME}
+
+    def form_valid(self, form):
+        opts = {
+            "use_https": self.request.is_secure(),
+            "token_generator": self.token_generator,
+            "from_email": self.from_email,
+            "email_template_name": self.email_template_name,
+            "subject_template_name": self.subject_template_name,
+            "request": self.request,
+            "html_email_template_name": self.html_email_template_name,
+            "extra_email_context": self.extra_email_context,
+            "domain_override": settings.SITE_DOMAIN,
+        }
+        form.save(**opts)
+        return FormView.form_valid(self, form)
